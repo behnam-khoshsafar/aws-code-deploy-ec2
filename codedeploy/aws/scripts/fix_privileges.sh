167840ff0ebbc0847e41eda96f5dea134533e4bb
behnam-khoshsafar <b.khoshsafar@gmail.com>
2022-05-26 19:20:13 +0200

    add validation step in codedeploy config

--- codedeploy/aws/scripts/fix_privileges.sh
+++ codedeploy/aws/scripts/fix_privileges.sh
@@ -1,3 +1,4 @@
 chmod +x /usr/local/codedeployresources/*.jar
 chmod +x /usr/local/codedeployresources/aws/scripts/server_start.sh
 chmod +x /usr/local/codedeployresources/aws/scripts/server_stop.sh
+chmod +x /usr/local/codedeployresources/aws/scripts/server_validation.sh
