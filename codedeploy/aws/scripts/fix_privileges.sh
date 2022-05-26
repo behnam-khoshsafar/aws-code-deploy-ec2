498e09a00fe1f52f37236ea341bccd9905b5e153
behnam-khoshsafar <b.khoshsafar@gmail.com>
2022-05-26 18:43:23 +0200

    refactor codedeploy config

--- codedeploy/aws/scripts/fix_privileges.sh
+++ codedeploy/aws/scripts/fix_privileges.sh
@@ -1,3 +1,3 @@
 chmod +x /usr/local/codedeployresources/*.jar
-chmod +x /usr/local/codedeployresources/server_start.sh
-chmod +x /usr/local/codedeployresources/server_stop.sh
+chmod +x /usr/local/codedeployresources/aws/scripts/server_start.sh
+chmod +x /usr/local/codedeployresources/aws/scripts/server_stop.sh
