bc18f808c1b7a96e305c0cfba95b7ddbfa3b4f60
behnam-khoshsafar <b.khoshsafar@gmail.com>
2022-05-26 15:47:52 +0200

    codedeploy config

---
+++ codedeploy/aws/script/fix_privileges.sh
@@ -0,0 +1,3 @@
+chmod +x /usr/local/codedeployresources/* jar
+chmod +x /usr/local/codedeployresources/server_start.sh
+chmod +x /usr/local/codedeployresources/server_stop.sh
---
+++ codedeploy/aws/script/server_start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /usr/local/codedeployresources
+sudo java -jar -Dserver.port=80 *.jar
